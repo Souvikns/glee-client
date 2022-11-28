f51605c2db2fcec1be3855c730db70e6aeb0a169
souvik <souvikde.ns@gmail.com>
2022-11-28 19:52:32 +0530

    Update websocket.ts

--- lib/adapters/websocket.ts
+++ lib/adapters/websocket.ts
@@ -1,13 +1,63 @@
-import Adapter from "../core/adapter";
+import Adapter from '../core/adapter'
+import ws from 'ws'
 
+
+interface Client {
+  channel: string
+  client: ws
+  binddings?: any
+}
 export default class WebsocketAdapter extends Adapter {
+  private _clients: Array<Client> = []
   name(): string {
-    return "WebSocket Adapter";
+    return 'WebSocket Adapter'
   }
 
   async connect(): Promise<void> {
-    await this._connect();
+    await this._connect()
   }
 
-  private async _connect() {}
+  private async _connect() {
+    const channelsOnThisServer = this.getWsChannels()
+
+    for (const channel of channelsOnThisServer) {
+      const wsBindings = this.parsedAsyncAPI.channel(channel).binding('ws')
+      const url = new URL(
+        this.AsyncAPIServer.url() + channel
+      )
+      this._clients.push({
+        channel: channel,
+        client: new ws(url),
+        binddings: this.parsedAsyncAPI.channel(channel).binding('ws')
+      })
+
+    }
+
+    for (const {client, channel} of this._clients) {
+      client.on("open", () => {
+      })
+    }
+  }
+
+  private getWsChannels() {
+    const channels = []
+    for (const channel of this.channelNames) {
+      if (this.parsedAsyncAPI.channel(channel).hasBinding('ws')) {
+        if (this.parsedAsyncAPI.channel(channel).hasServers()) {
+          if (
+            this.parsedAsyncAPI
+              .channel(channel)
+              .servers()
+              .includes(this.serverName)
+          ) {
+            channels.push(channel)
+          }
+        } else {
+          channels.push(channel)
+        }
+      }
+    }
+
+    return channels
+  }
 }
